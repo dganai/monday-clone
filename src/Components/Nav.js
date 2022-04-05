38ce1769b21f27a751a33daaea5d38134e36a4e6
dganai <dinaganai@gmail.com>
2022-04-05 19:32:38 -0400

    nav component setup for routes

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,4 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import logo from '../Images/default_icon.jpeg';
+
 const Nav = () => {
-  return <div>Nav</div>;
+  const navigate = useNavigate();
+  return (
+    <nav>
+      <div className="logo-container">
+        <img src={logo} alt={logo} />
+      </div>
+      <div className="controls-container">
+        <div className="icon" onClick={() => navigate('/ticket')}>
+          ➕
+        </div>
+        <div className="icon" onClick={() => navigate('/')}>
+          «
+        </div>
+      </div>
+    </nav>
+  );
 };
 export default Nav;
