6e8f74c19b9cd670878ae001ba02f107dfd88429
dganai <dinaganai@gmail.com>
2022-04-05 19:11:54 -0400

    adding TicketCard component to Dashboard file

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,4 +1,14 @@
+import TicketCard from '../Components/TicketCard';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      Dashboard
+      <h1>My Project</h1>
+      <div className="ticket-container">
+        <TicketCard />
+      </div>
+    </div>
+  );
 };
 export default Dashboard;
