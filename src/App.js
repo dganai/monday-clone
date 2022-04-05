4faf0caacff6c3c4cb1a8640e29745198b9d8725
dganai <dinaganai@gmail.com>
2022-04-05 19:08:25 -0400

    route setup for dashboard and ticketpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './Pages/Dashboard';
+import TicketPage from './Pages/TicketPage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/ticket" element={<TicketPage />} />
+          <Route path="/ticket/:id" element={<TicketPage editMode={true} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
