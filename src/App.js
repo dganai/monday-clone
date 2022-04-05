30f3602ad03714ba3c9d6de6df295bac208f5ecd
dganai <dinaganai@gmail.com>
2022-04-05 19:09:30 -0400

    adding route for Nav to be visisible on all pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './Pages/Dashboard';
 import TicketPage from './Pages/TicketPage';
+import Nav from './Components/Nav';
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Nav />
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/ticket" element={<TicketPage />} />
