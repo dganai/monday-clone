58c60e75c9ba8ebd5be02ae9e788f850baec75b3
dganai <dinaganai@gmail.com>
2022-04-05 20:08:41 -0400

    adding dummy data

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,6 +1,33 @@
 import TicketCard from '../Components/TicketCard';
 
 const Dashboard = () => {
+  const tickets = [
+    {
+      category: 'Q1 2022',
+      color: 'red',
+      title: 'Test 101',
+      owner: 'Dina Ganai',
+      avatar: 'https://avatars.githubusercontent.com/u/91380160?v=4',
+      status: 'done',
+      priority: 5,
+      progress: 40,
+      description: 'Test data to see how this goes',
+      timestamp: '2022-04-05',
+    },
+    {
+      category: 'Q1 2022',
+      color: 'red',
+      title: 'Building Something',
+      owner: 'Dina Ganai',
+      avatar: 'https://avatars.githubusercontent.com/u/91380160?v=4',
+      status: 'done',
+      priority: 7,
+      progress: 65,
+      description: 'Make a video on how to build something',
+      timestamp: '2022-04-01',
+    },
+  ];
+
   return (
     <div className="dashboard">
       <h1>My Project</h1>
