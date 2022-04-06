d803e5b488bd39b8d5077ffa3900f3f46f9a4f8c
dganai <dinaganai@gmail.com>
2022-04-05 20:40:00 -0400

    functionality for avatar to show on ticketCard

--- src/Components/AvatarDisplay.js
+++ src/Components/AvatarDisplay.js
@@ -1,6 +1,15 @@
+import blankAvatar from '../Images/blank-avatar.png';
+
 const AvatarDisplay = () => {
-  return <div className="avatar-container">
-    
-  </div>;
+  return (
+    <div className="avatar-container">
+      <div className="image-container">
+        <img
+          src={ticket.avatar ? ticket.avatar : blankAvatar}
+          alt={'photo of ' + ticket.owner}
+        />
+      </div>
+    </div>
+  );
 };
 export default AvatarDisplay;
