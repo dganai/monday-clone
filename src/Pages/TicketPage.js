4fbfc144d213c9acb06322ddf266432db10a324f
dganai <dinaganai@gmail.com>
2022-04-06 14:27:20 -0400

    functionality for form of ticket page, import state

--- src/Pages/TicketPage.js
+++ src/Pages/TicketPage.js
@@ -1,4 +1,38 @@
+import { useState } from "react";
+
+
 const TicketPage = () => {
-  return <div>TicketPage</div>;
+  const [formData, setFormData] = useState({
+    s
+  })
+  const editmMode = false;
+
+  const handleSubmit = () => {
+    console.log('submitted');
+  };
+  const handleChange = () => {
+    console.log('changed');
+  };
+
+  return (
+    <div className="ticket">
+      <h1>{editmMode ? 'Update your Ticket' : 'Create a Ticket'}</h1>
+      <div className="ticket-container">
+        <form onSubmit={handleSubmit}>
+          <section>
+            <label htmlFor="title">Title</label>
+            <input
+              id="title"
+              name="title"
+              type="text"
+              onChange={handleChange}
+              required={true}
+              value={formData.title}
+            />
+          </section>
+        </form>
+      </div>
+    </div>
+  );
 };
 export default TicketPage;
