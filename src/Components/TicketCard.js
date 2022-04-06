9407da8657503ebf265f438027c6c1defd15ee16
dganai <dinaganai@gmail.com>
2022-04-05 20:01:28 -0400

    functionality for ticketcard to show components

--- src/Components/TicketCard.js
+++ src/Components/TicketCard.js
@@ -1,4 +1,23 @@
+import { Link } from 'react-router-dom';
+import AvatarDisplay from './AvatarDisplay';
+import StatusDisplay from './StatusDisplay';
+import PriorityDisplay from './PriorityDisplay';
+import ProgressDisplay from './ProgressDisplay';
+import DeleteBlock from './DeleteBlock';
+
 const TicketCard = () => {
-  return <div className="ticket-card"></div>;
+  return (
+    <div className="ticket-card">
+      <Link>
+      <div className="ticket-color"></div>
+        <h3>title</h3>
+        <AvatarDisplay />
+        <StatusDisplay />
+        <PriorityDisplay />
+        <ProgressDisplay />
+      </Link>
+      <DeleteBlock />
+    </div>
+  );
 };
 export default TicketCard;
