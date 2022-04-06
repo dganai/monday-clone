e5f6995d66e9ec9729e9644c36c4b4954a50c9cc
dganai <dinaganai@gmail.com>
2022-04-06 14:06:39 -0400

    functionality for delete block

--- src/Components/DeleteBlock.js
+++ src/Components/DeleteBlock.js
@@ -1,4 +1,13 @@
 const DeleteBlock = () => {
-  return <div>DeleteBlock</div>;
+  const deleteTicket = () => {
+    console.log('deleted');
+  };
+  return (
+    <div className="delete-block">
+      <div className="delete-icon" onClick={deleteTicket}>
+        ✖
+      </div>
+    </div>
+  );
 };
 export default DeleteBlock;
