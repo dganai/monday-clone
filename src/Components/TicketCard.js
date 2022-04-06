1e5405da577b2d6665ff8d86d52314a7b797ad0b
dganai <dinaganai@gmail.com>
2022-04-06 14:20:27 -0400

    functionality for each category to have it's own color

--- src/Components/TicketCard.js
+++ src/Components/TicketCard.js
@@ -9,14 +9,14 @@ const TicketCard = ({ color, ticket }) => {
   return (
     <div className="ticket-card">
       <Link to={`/ticket/${ticket.documentId}`} id="link">
-        <div className="ticket-color"></div>
+        <div className="ticket-color" style={{ backgroundColor: color }}></div>
         <h3>{ticket.title}</h3>
         <AvatarDisplay ticket={ticket} />
         <StatusDisplay status={ticket.status} />
         <PriorityDisplay priority={ticket.priority} />
         <ProgressDisplay progress={ticket.progress} />
+        <DeleteBlock />
       </Link>
-      <DeleteBlock />
     </div>
   );
 };
