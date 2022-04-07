7b6c94dc5c228327ba93d1a4f35dd1d51bd5444f
dganai <dinaganai@gmail.com>
2022-04-07 16:09:17 -0400

    addition of env file

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ app.use(express.json());
 
 // url to communicate with backend
 // url is for making tickets in task collection
-const url =
-  'https://8f7cbd20-d511-4eac-8ce2-333695b6f55b-us-east1.apps.astra.datastax.com/api/rest/v2/namespaces/tickets/collections/tasks';
-const token =
-  'AstraCS:DAoiaZuYUMmiqjyFEZLOfMHX:33fcda178f0b71ca7d47ff07dd030a0da22468e8f6d185c0c4de75a81963ff07';
+const url = process.env.URL;
+const token = process.env.ASTRA_TOKEN;
 
 // use data from db to get all tickets
 app.get('/tickets', async (req, res) => {
