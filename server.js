cc99dc1c3b88f6c5aca8cc81df95414a173167a7
dganai <dinaganai@gmail.com>
2022-04-06 21:14:28 -0400

    post request for creating tickets with axios

--- server.js
+++ server.js
@@ -8,10 +8,26 @@ const axios = require('axios');
 const app = express();
 
 // url to communicate with backend
+// url is for making tickets in collection
 const url =
   'https://8f7cbd20-d511-4eac-8ce2-333695b6f55b-us-east1.apps.astra.datastax.com/api/rest/v2/namespaces/tickets/collections';
 const token =
   'AstraCS:DAoiaZuYUMmiqjyFEZLOfMHX:33fcda178f0b71ca7d47ff07dd030a0da22468e8f6d185c0c4de75a81963ff07';
+
+// creating tickets
+app.post('/tickets', async (req, res) => {
+  const formData = req.body.formData;
+
+  // once we have form data, make axios post req to database
+  const options = {
+    method: 'POST',
+    headers: {
+      Accepts: 'application/json',
+      'X-Cassandra-Token': token,
+      'Content-Type': 'application/json',
+    },
+  };
+});
 
 // listen to changes on the PORT
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
