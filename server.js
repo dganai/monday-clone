f78ac2272671805da16fcb85709852e896778307
dganai <dinaganai@gmail.com>
2022-04-06 21:10:36 -0400

    connecting url database from datastax

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ const axios = require('axios');
 
 // call express and assign to new var
 const app = express();
+
+// url to communicate with backend
+const url =
+  'https://8f7cbd20-d511-4eac-8ce2-333695b6f55b-us-east1.apps.astra.datastax.com/api/rest/v2/namespaces/tickets/collections';
+const token =
+  'AstraCS:DAoiaZuYUMmiqjyFEZLOfMHX:33fcda178f0b71ca7d47ff07dd030a0da22468e8f6d185c0c4de75a81963ff07';
+
+// listen to changes on the PORT
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
